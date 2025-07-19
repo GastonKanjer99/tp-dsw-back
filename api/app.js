1c00ef8826a786ca25d94ed15da11d730d710ee8
GastonKanjer99 <gaston_kanjer@outlook.com>
2025-07-19 17:13:26 -0300

    Se modifica estructura y se agrega llamado para prueba de creacion de DB

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes');
-
+const db = require('./src/models');
 const app = express();
 const PORT = 3000;
 
@@ -10,12 +10,23 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-const db = require('./src/models');
 
 // Sincroniza modelos con la base de datos
 db.sequelize.sync({ alter: true }) // o { force: true } para forzar el borrado y recreación
   .then(() => console.log('Tablas sincronizadas con éxito'))
   .catch(err => console.error('Error al sincronizar la DB:', err));
+
+//   app.get('/races', async (req, res) => {
+//   try {
+//     const races = await db.Race.findAll();
+//     res.json(races);
+//   } catch (error) {
+//     console.error('Error al obtener races:', error);
+//     res.status(500).json({ error: 'Error al obtener races' });
+//   }
+// });
+
+
   
 
 app.listen(PORT, () => {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/races', require('./races'));
+router.use('/races', require('./races')); //Llamo a razas: api/races/api O /api/races. API o DB respectivamente  
 router.use('/classes', require('./classes'));
 router.use('/backgrounds', require('./backgrounds'));
 
